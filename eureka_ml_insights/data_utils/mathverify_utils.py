5e32c4d4c1692d431d8267df5421f4fa70adcbee
Jiwan Chung <jiwan.chung.research@gmail.com>
2025-08-13 03:22:03 +0000

    minor fix

--- eureka_ml_insights/data_utils/mathverify_utils.py
+++ eureka_ml_insights/data_utils/mathverify_utils.py
@@ -79,6 +79,7 @@ def extract_boxed_answer(reasoning: str):
         return out.removesuffix(".")
     # matches = re.findall(r"\\boxed\{(.*?)}", reasoning)
     # output = matches[-1] if matches else "Unknown"
+
     output = extract_last_boxed(reasoning)
     if output is None:
         # openvlthinker
@@ -98,7 +99,132 @@ def extract_boxed_answer(reasoning: str):
         output = (
             output.strip().removeprefix("Final Answer:").removeprefix("Answer:").strip()
         )
+
+    if output is None:
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"<CONCLUSION>(.+?)</CONCLUSION>"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        temp = matches[-1].removeprefix(".") if matches else None
+        if temp is not None:
+            pattern = r"is\s+([^.]+)\."
+
+            # find all matches, take last one
+            matches = re.findall(pattern, temp, flags=re.IGNORECASE)
+            output = matches[-1].strip() if matches else None
+            if output is None:
+                output = temp
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Therefore, the final answer is\s+\\\(\s*(.+?)\s*\\\)"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Therefore, the answer is\s+\\\(\s*(.+?)\s*\\\)"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Thus, the final answer is\s+\\\(\s*(.+?)\s*\\\)"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Thus, the answer is\s+\\\(\s*(.+?)\s*\\\)"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        pattern = r"<\|begin_of_box\|>(.+?)<\|end_of_box\|>"
+        matches = re.findall(pattern, reasoning, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1]
+        pattern = r"Answer: (.+?)$"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        if last.startswith("Therefore,"):
+            pattern = r"is (.+?)$"
+            matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+            output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Therefore, the final answer is\s+\s*(.+?)\s*$"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"Therefore, the answer is\s+\s*(.+?)\s*$"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is None:
+        # glm-v4.1
+        last = reasoning.strip().split("\n")[-1].removesuffix(".")
+        pattern = r"\*\*Final Answer:\*\*\s*(.+?)\s*$"
+        matches = re.findall(pattern, last, flags=re.IGNORECASE | re.DOTALL)
+        output = matches[-1].removeprefix(".") if matches else None
+    if output is not None and "=" in output:
+        output = output.split("=")[-1].strip()
+    if output is not None:
+        output = output.removesuffix("degrees").strip()
     return output
+
+
+# def extract_boxed_answer(reasoning: str):
+#     """
+#     Extract the content inside the last \\boxed{...} in the reasoning.
+#     """
+#     if reasoning is None:
+#         return None
+#     mc_answer = reasoning.strip()
+#     mc_labels = [*LABELS_TRUE, *LABELS_FALSE]
+#     if mc_answer.lower() in mc_labels:
+#         return mc_answer
+#     elif reasoning.split("\n")[0].lower() in [
+#         *mc_labels,
+#         *[f"{v}." for v in mc_labels],
+#     ]:
+#         return reasoning.split("\n")[0].removesuffix(".")
+#     elif reasoning.split("\n")[0].lower() in [
+#         *[f"\\boxed{{{v}}}" for v in mc_labels],
+#         *[f"\\boxed{{{v}}}." for v in mc_labels],
+#     ]:
+#         out = extract_last_boxed(reasoning.split("\n")[0])
+#         if out is None:
+#             return None
+#         return out.removesuffix(".")
+#     # matches = re.findall(r"\\boxed\{(.*?)}", reasoning)
+#     # output = matches[-1] if matches else "Unknown"
+#     output = extract_last_boxed(reasoning)
+#     if output is None:
+#         # openvlthinker
+#         matches = re.findall(r"<answer>(.*?)</answer>", reasoning)
+#         output = matches[-1] if matches else None
+#     if output is None:
+#         # internvl-4b
+#         pattern = r"The answer is:\s*(.+?)"
+#         matches = re.findall(pattern, reasoning, flags=re.IGNORECASE | re.DOTALL)
+#         output = matches[-1].removeprefix(".") if matches else None
+#     if output is None:
+#         pattern = r"The correct answer is:\s*(.+?)"
+#         matches = re.findall(pattern, reasoning, flags=re.IGNORECASE | re.DOTALL)
+#         output = matches[-1].removeprefix(".") if matches else None
+#     if output is not None:
+#         # vision-r1: <answer> Final Answer:{}</answer>
+#         output = (
+#             output.strip().removeprefix("Final Answer:").removeprefix("Answer:").strip()
+#         )
+#     return output
 
 
 def _parse(x):
